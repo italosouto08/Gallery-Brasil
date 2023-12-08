0e512ebe77289df329f2ef0447d0eb76ff4324ab
Ítalo Souto <italoivo14@hotmail.com>
2023-12-08 10:08:28 -0300

    first commit

---
+++ app/data.tsx
@@ -0,0 +1,33 @@
+export const sculptureList = [{
+    name: 'Abaporu',
+    artist: 'Marta Colvin AndradeTarsila do Amaral',
+    description: 'Entregue como um presente ao marido Oswald de Andrade, ele serviu de inspiração para o Manifesto Antropofágico. A obra foi comprada em 1995 pelo colecionador argentino Eduardo Costantini, na época, por 2,5 milhões de dólares.',
+    url: 'https://s2-casaejardim.glbimg.com/uELBEn63Yz4sJCTCpguUdHh43RA=/0x0:793x931/1000x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_a0b7e59562ef42049f4e191fe476fe7d/internal_photos/bs/2023/F/7/p14ER6S0emftcdACJQ5g/abaporu.jpg',  
+  }, {
+    name: 'Guerra e Paz',
+    artist: 'Candido Portinari',
+    description: 'A sede da Assembleia Geral da Organização das Nações Unidas, em Nova York (EUA), guarda os painéis Guerra e Paz, de Candido Portinari. Comercializado na década de 1990, as obras de arte foram um presente do governo brasileiro à ONU.',
+    url: 'https://s2-casaejardim.glbimg.com/szPUkPfEu56_qKQCF-mH_LSleSo=/0x0:1170x530/1000x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_a0b7e59562ef42049f4e191fe476fe7d/internal_photos/bs/2023/4/B/adMAE7SLO62EAeDMUkNg/guerra-e-paz.jpg',
+  }, {
+    name: 'A Lua',
+    artist: 'Tarsila do Amaral',
+    description: 'A obra faz parte da ala “Paris 1920”, que retrata a retomada da atmosfera artística e cosmopolita da capital francesa após a Primeira Guerra Mundial. A Lua, pintada em 1928, está exposta junto a grandes nomes da época, como Fernand Léger, Constantin Brancusi e Pablo Picasso."',
+    url: 'https://s2-casaejardim.glbimg.com/AqbOwIN6Wjg8Jwizm_dF2JC63t4=/0x0:800x800/1000x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_a0b7e59562ef42049f4e191fe476fe7d/internal_photos/bs/2023/E/S/7DMNdmRa6lairdikYPRQ/lua-tarsila-do-amaral.jpg',
+  }, {
+    name: 'Babel',
+    artist: 'Cildo Meireles',
+    description: 'Babel é uma das obras emblemáticas do artista plástico carioca Cildo Meireles, parte da exposição permanente do Tate Modern, em Londres. A torre tem 5 m de altura e foi construída com 800 aparelhos de rádio, cada um sintonizado em uma estação diferente no menor nível do volume.',
+    url: 'https://s2-casaejardim.glbimg.com/lyWM0HjPIB5QM0q_ew7iYydRtrY=/0x0:1200x800/1000x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_a0b7e59562ef42049f4e191fe476fe7d/internal_photos/bs/2023/G/6/WC4yBwSIqp5FNLnPhZjw/babel-cildo-meireles.jpg',
+  }, {
+    name: 'Mural "Nações Unidas Nova York"',
+    artist: 'Eduardo Kobra',
+    description: 'Um dos pontos altos recentes de sua carreira é o mural de 400 m², também na sede da Assembleia Geral da Organização das Nações Unidas, em Nova York. No grafite, um pai entrega o planeta para sua filha, levantando a questão: qual será o mundo que deixaremos para as futuras gerações?',
+    url: 'https://s2-casaejardim.glbimg.com/EHpMVpLq4xNyV85vZ3upDPtUeew=/0x0:2500x1667/1000x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_a0b7e59562ef42049f4e191fe476fe7d/internal_photos/bs/2023/d/u/baEoPIQdq6wSd5ORg65w/mural-nacoes-unidas-kobra.jpg',
+  }, {
+    name: 'Swimming Pool',
+    artist: 'Adriana Varejão',
+    description: 'Em 2007, Adriana Varejão expôs pela primeira vez no Museu de Arte Contemporânea Hara, em Tóquio, no Japão. Foram mais de 20 obras, incluindo uma reprodução em tamanho menor do mural de azulejos presente no Museu de Arte Moderna de São Paulo (MAM), com a taxonomia de 50 tipos de plantas alucinógenas. Mas a peça escolhida para a coleção permanente do museu pela curadora Yoko Uchida foi a tela Swimming Pool, de 2005. O quadro chegou a ser cobiçado também pela Fundação Cartier, em Paris.',
+    url: 'https://s2-casaejardim.glbimg.com/W-sXDxtHBViQ_35ZJKC3oZReDFo=/0x0:810x637/1000x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_a0b7e59562ef42049f4e191fe476fe7d/internal_photos/bs/2023/N/f/hRONJ1RHGRhozuxnp5JQ/adriana-varejao-swimming-pool.jpg',
+  }];
+  
+
